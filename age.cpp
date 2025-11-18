0d7d7fe5ad262c1a3d52c4eb777f5d04898a945e
01Sakshibhaleghare <sakshibhaleghare00@gmail.com>
2025-11-18 11:29:41 +0530

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cout << "Enter your age: ";      // asks the user for input
+    cin >> age;                      // stores the input in the variable 'age'
+
+    // prints multiple lines with '\n'
+    cout << "Your age is: " << age << " you are getting old\n" << endl;
+    return 0;
+}
